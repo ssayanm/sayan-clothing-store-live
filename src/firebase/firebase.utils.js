5d91aeca40c5747890b043adaaf38ca4cf322ced
Sayan Mukherjee <hello@sayanmukherjee.com>
2019-12-04 16:35:46 +0530

    updatied a small issue

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -29,7 +29,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 				...additionalData
 			})
 		}catch(error){
-			console.log('error creting user', error.message)
+			console.log('error creating user', error.message)
 		}
 	}
 	return userRef;
