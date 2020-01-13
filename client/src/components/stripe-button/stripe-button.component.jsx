70bdb72225aaaa6b3cbda585dab6f7f2ba97467e
Sayan Mukherjee <sayan@dev-server.localhost>
2020-01-13 08:37:03 +0530

    added clearcart to clear the cart values after successful payment

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -4,7 +4,7 @@ import StripeCheckout from "react-stripe-checkout";
 import axios from "axios";
 import { clearCart } from "../../redux/cart/cart.actions";
 
-const StripeCheckoutButton = ({ price }) => {
+const StripeCheckoutButton = ({ price, clearCart }) => {
   const priceForStripe = price * 100;
   const publishableKey = "pk_test_zMwyyMUHg6wlV1CXpNfS2wG200WiXpNCMa";
 
@@ -19,6 +19,7 @@ const StripeCheckoutButton = ({ price }) => {
     })
       .then(response => {
         alert("succesful payment");
+        clearCart();
       })
       .catch(error => {
         console.log("Payment Error: ", error);
