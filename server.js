bc15e97e73c7a152f3ce288fef8ddc9e89ab1a58
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-01-21 14:48:46 +0530

    updated server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require("path");
-const compression = require("compression");
 const enforce = require("express-sslify");
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
@@ -12,32 +11,12 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-// app.use(compression());
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(enforce.HTTPS({ trustProtoHeader: true }));
-// app.use(cors());
-
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "client/build")));
-
-//   app.get("*", function(req, res) {
-//     res.sendFile(path.join(__dirname, "client/build", "index.html"));
-//   });
-// }
-
-// app.listen(port, error => {
-//   if (error) throw error;
-//   console.log("Server running on port " + port);
-// });
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(compression);
-  app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, "client/build")));
 
   app.get("*", function(req, res) {
@@ -47,10 +26,10 @@ if (process.env.NODE_ENV === "production") {
 
 app.listen(port, error => {
   if (error) throw error;
-  console.log("Server is running on port " + port);
+  console.log("Server running on port " + port);
 });
 
-app.get("/serviceWorker.js", (req, res) => {
+app.get("/service-worker.js", (req, res) => {
   res.sendFile(path.resolve(__dirname, "..", "build", "service-worker.js"));
 });
 
@@ -58,7 +37,7 @@ app.post("/payment", (req, res) => {
   const body = {
     source: req.body.token.id,
     amount: req.body.amount,
-    currency: "inr"
+    currency: "usd"
   };
 
   stripe.charges.create(body, (stripeErr, stripeRes) => {
